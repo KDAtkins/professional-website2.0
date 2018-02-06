dd1f9af548b77f31d195f26f7dbbb1b648e71d24
Kevin Atkins <kaitoxas@gmail.com>
2018-02-06 11:27:46 -0800

    made comments regarding layout of index page

--- public_html/index.php
+++ public_html/index.php
@@ -3,12 +3,30 @@
 	<head>
 		<meta charset="UTF-8">
 
+		<!--Bootstrap Stylesheet-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 		<!--Responsive meta tag-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<title>TBD</title>
 	</head>
 	<body>
+		<!-- Navbar
+		Title Link: TBD
+		Link-0: Home
+		Link-1: About
+		Link-2: Portfolio
+		Link-3: Contact
+		-->
+
+		<!-- Home -->
+
+		<!-- About -->
+
+		<!-- Portfolio -->
+
+		<!-- Contact -->
 
 	</body>
 	<footer>
